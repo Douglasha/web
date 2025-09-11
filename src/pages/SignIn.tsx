6dca7639e00e1f4475dde08bbb1e3f62a42c60d4
douglasha <douglas.henriqueamaral@gmail.com>
2025-09-10 21:40:36 -0300

    Criando Componente de Input

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,23 @@
+import { Input } from "../components/Input.tsx"
+
 export function SignIn() {
-  return <h1>Sign In</h1>;
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+      />
+
+    </form>
+  )
 }
