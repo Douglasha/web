7de29e0ec9586c23dbca538773b9df86b5549fb4
douglasha <douglas.henriqueamaral@gmail.com>
2025-09-17 20:45:29 -0300

    Criando a Rota de Refund

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router"
 
 import { AuthRoutes } from "./AuthRoutes.tsx"
+import { EmployeeRoutes } from "./EmployeeRoutes.tsx"
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <EmployeeRoutes />
     </BrowserRouter>
   )
 }
