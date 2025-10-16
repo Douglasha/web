bdb421cf7b404538334b087a26260e8859e19301
douglasha <douglas.henriqueamaral@gmail.com>
2025-10-16 20:04:54 -0300

    Utilizando zod

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,27 +1,61 @@
-import { useState } from "react"
-import { Input } from "../components/Input.tsx"
-import { Button } from "../components/Button.tsx"
+import { useState } from "react";
+import { z, ZodError } from "zod";
+import { Input } from "../components/Input.tsx";
+import { Button } from "../components/Button.tsx";
+import { fi } from "zod/locales";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "A senha deve ter no mínimo 6 caracteres" }),
+    passwordConfirm: z
+      .string()
+      .min(6, { message: "A senha deve ter no mínimo 6 caracteres" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não coincidem",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUP() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [passwordConfirm, setPasswordConfirm] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function onSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    alert("Formulário enviado")
-    console.log( name, email, password, passwordConfirm )
-    setIsLoading(false)
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+      alert("Erro ao cadastrar usuário");
+    } finally {
+      setIsLoading(false);
+    }
   }
+
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="Name"
         placeholder="seu nome"
-        onChange={e => setName(e.target.value)}
+        onChange={(e) => setName(e.target.value)}
       />
 
       <Input
@@ -29,16 +63,15 @@ export function SignUP() {
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        onChange={e => setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
       />
-
 
       <Input
         required
         legend="Senha"
         type="password"
         placeholder="123456"
-        onChange={e => setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <Input
@@ -46,18 +79,19 @@ export function SignUP() {
         legend="Confirme a Senha"
         type="password"
         placeholder="123456"
-        onChange={e => setPasswordConfirm(e.target.value)}
+        onChange={(e) => setPasswordConfirm(e.target.value)}
       />
 
-      <Button type="submit" isLoading={isLoading}>Cadastrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Cadastrar
+      </Button>
 
-      <a 
+      <a
         href="/"
         className="text-sm font-semibold text-center text-gray-100 mt-10 mb-4 hover:text-green-800 transition ease-linear"
       >
-        Já tenho uma conta 
+        Já tenho uma conta
       </a>
-
     </form>
-  )
+  );
 }
