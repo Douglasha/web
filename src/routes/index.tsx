13261629f30c92cdea7a44b26b0f7b64093d0ae0
douglasha <douglas.henriqueamaral@gmail.com>
2025-10-15 21:41:24 -0300

    Componente de Loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading.tsx";
+
 import { AuthRoutes } from "./AuthRoutes.tsx";
 import { ManagerRoutes } from "./ManagerRoutes.tsx";
 import { EmployeeRoutes } from "./EmployeeRoutes.tsx";
 
+const isLoading = true; // TODO: replace with actual loading state
+
 export function Routes() {
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
