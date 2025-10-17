b51b26d87e01ec7865314f6b7a6d926c220864b4
douglasha <douglas.henriqueamaral@gmail.com>
2025-10-17 19:57:59 -0300

    Validação dos dados usando o Zod

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,46 +1,67 @@
-import { useState } from "react"
-import { Input } from "../components/Input.tsx"
-import { Button } from "../components/Button.tsx"
+import { useActionState } from "react";
+import { z, ZodError } from "zod";
+
+import { Input } from "../components/Input.tsx";
+import { Button } from "../components/Button.tsx";
+
+const signInSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido" }),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve ter no mínimo 6 caracteres" }),
+});
 
 export function SignIn() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+  const [state, formAction, isLoading] = useActionState(signIn, null);
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    alert("Formulário enviado")
-    console.log( email, password )
-    setIsLoading(false)
+  async function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+
+      console.log(data);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message };
+      }
+
+      return { message: "Não foi possível entrar!" };
+    }
   }
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input
+        name="email"
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        onChange={e => setEmail(e.target.value)}
       />
 
-
       <Input
+        name="password"
         required
         legend="Senha"
         type="password"
         placeholder="123456"
-        onChange={e => setPassword(e.target.value)}
       />
 
-      <Button type="submit" isLoading={isLoading}>Entrar</Button>
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.message}
+      </p>
 
-      <a 
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+
+      <a
         href="/signup"
         className="text-sm font-semibold text-center text-gray-100 mt-10 mb-4 hover:text-green-800 transition ease-linear"
       >
-        Criar Conta 
+        Criar Conta
       </a>
-
     </form>
-  )
+  );
 }
